ff62f91f2d15219ae494ed8ca7da82d00a6b5359
Christoph Griesbacher <christoph.griesbacher@student.tugraz.at>
2024-01-18 10:39:41 +0100

    cfgs

--- tools/train_with_pretrained.sh
+++ tools/train_with_pretrained.sh
@@ -2,24 +2,25 @@
 # Finetune a single detector on an arbitrary dataset
 
 
-DATASET=avltruck
-MODEL=pvrcnnpp_sn2rooftop
-EXTRA_TAG=D6_1epochs_sn2rooftop_ft_D6_50epochs
-SUBSAMPLE=6
-PRETRAINED=/home/cgriesbacher/thesis/3DTrans/output/avltruck_models/pvrcnnpp/D6_50epochs/ckpt/checkpoint_epoch_50.pth
+DATASET=zod
+MODEL=pvrcnnpp_ros_rbds
+EXTRA_TAG=D16_10epochs_rbds0.5_ft_D6_50epochs_ros
+PRETRAINED=/home/cgriesbacher/thesis/3DTrans/output/zod_models/pvrcnnpp_ros/D6_50epochs/ckpt/checkpoint_epoch_50.pth
 
-EPOCHS=1
-CKPT_SAVE_INTERVAL=1
-BATCHSIZE=4
+EPOCHS=10
+CKPT_SAVE_INTERVAL=10
+BATCHSIZE=16
 WORKERS=4
+
+NUM_GPUS=2
+export CUDA_VISIBLE_DEVICES=0,1
 
 CONFIG_FILE=${DATASET}_models/$MODEL.yaml
 
 
 #single gpu training
-python train.py --cfg_file cfgs/$CONFIG_FILE --extra_tag $EXTRA_TAG  --epochs $EPOCHS --subsample $SUBSAMPLE --ckpt_save_interval $CKPT_SAVE_INTERVAL --batch_size $BATCHSIZE --workers $WORKERS --pretrained_model $PRETRAINED
+#python train.py --cfg_file cfgs/$CONFIG_FILE --extra_tag $EXTRA_TAG  --epochs $EPOCHS --ckpt_save_interval $CKPT_SAVE_INTERVAL --batch_size $BATCHSIZE --workers $WORKERS --pretrained_model $PRETRAINED
 
 #multi gpu training
-#NUM_GPUS=1
-#bash scripts/dist_train.sh $NUM_GPUS --cfg_file cfgs/$CONFIG_FILE --extra_tag $EXTRA_TAG  --epochs $EPOCHS --subsample $SUBSAMPLE --ckpt_save_interval $CKPT_SAVE_INTERVAL --batch_size $BATCHSIZE --workers $WORKERS
+bash scripts/dist_train.sh $NUM_GPUS --cfg_file cfgs/$CONFIG_FILE --extra_tag $EXTRA_TAG  --epochs $EPOCHS --ckpt_save_interval $CKPT_SAVE_INTERVAL --batch_size $BATCHSIZE --workers $WORKERS
 
